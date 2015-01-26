eab667726e2c9ebbeb1dc88c576fa0e0f0c36d46
Pan Zhiyong <zy.pan@wowtech-inc.com>
2015-01-26 20:14:12 +0800

    dynamic output file based on timestamp and public directory.

--- demo/src/com/skd/androidrecordingtest/utils/StorageUtils.java
+++ demo/src/com/skd/androidrecordingtest/utils/StorageUtils.java
@@ -2,10 +2,14 @@ package com.skd.androidrecordingtest.utils;
 
 import android.os.Environment;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class StorageUtils {
-	private static final String AUDIO_FILE_NAME = "audiorecordtest.wav";
-	private static final String VIDEO_FILE_NAME = "videorecordtest.3gp";
-	
+	private static final String AUDIO_EXT = "wav";
+	private static final String VIDEO_EXT = "3gp";
+
 	public static boolean checkExternalStorageAvailable() {
 		String state = Environment.getExternalStorageState();
 		if (Environment.MEDIA_MOUNTED.equals(state)) {
@@ -17,7 +21,19 @@ public class StorageUtils {
 	}
 	
 	public static String getFileName(boolean isAudio) {
-		String storageDir = Environment.getExternalStorageDirectory().getAbsolutePath();
-		return String.format("%s/%s", storageDir, (isAudio) ? AUDIO_FILE_NAME : VIDEO_FILE_NAME);
+		File path;
+		if (isAudio) {
+			path = Environment.getExternalStorageDirectory();
+			path = new File(path.getAbsolutePath() + "/Sound");
+		} else {
+			path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+			path = new File(path.getAbsolutePath() + "/Camera");
+		}
+		path.mkdirs();
+
+		return String.format("%s/%s.%s",
+				path.getAbsoluteFile(),
+				new SimpleDateFormat("yyyyMMdd_kkmmss_S").format(Calendar.getInstance().getTime()),
+				(isAudio) ? AUDIO_EXT : VIDEO_EXT);
 	}
 }
